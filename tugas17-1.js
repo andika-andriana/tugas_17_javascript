4527738c86b89254cdd97e84318615f19ffafb99
Andika Andriana <andriana.mia1@gmail.com>
2019-12-07 19:43:01 +0700

    Tugas 17 Javascript
    Tugas ini Saya kumpulkan untuk Niomic.

---
+++ tugas17-1.js
@@ -0,0 +1,11 @@
+function tugas17_1() {
+  var kata = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+  var kata1 = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+
+  console.log(kata.toLowerCase());
+  console.log(kata.toUpperCase());
+  console.log(kata1.toLowerCase());
+  console.log(kata1.toUpperCase());
+}
+
+tugas17_1()
---
+++ tugas17-2.js
@@ -0,0 +1,8 @@
+function tugas17_2() {
+  var lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+  console.log(lorem.charCodeAt(0), "-", lorem.charCodeAt(1), "-", lorem.charCodeAt(2), "-", lorem.charCodeAt(3), "-", lorem.charCodeAt(4), "-", lorem.charCodeAt(5));
+
+}
+
+tugas17_2()
